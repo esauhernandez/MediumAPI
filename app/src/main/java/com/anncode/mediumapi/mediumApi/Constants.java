018ca092539b67f16583f674085f60ed2438d5e2
Esau Hernández <esau.hernandez.contractor@bbva.com>
2018-08-16 15:33:08 -0500

    Initial commit

---
+++ app/src/main/java/com/anncode/mediumapi/mediumApi/Constants.java
@@ -0,0 +1,12 @@
+package com.anncode.mediumapi.mediumApi;
+
+/**
+ * Created by anahisalgado on 20/06/17.
+ */
+
+public class Constants {
+    public static final String VERSION = "/v1/";
+    public static final String ROOT_URL = "https://api.medium.com" + VERSION;
+    public static final String URL_GET_USER = "me/";
+    public static final String BEARER = "2f004932b47d2042965eed050030075447bc90a147e5a8ffe1fa17f30b0fffacf";
+}
